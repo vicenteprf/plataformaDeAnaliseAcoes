15cfd9a653242f30b041b14ac1d69a02a4254076
Vicente Ramos <vicenteprfilho09@gmail.com>
2026-05-30 17:48:43 -0300

    feat: setup backend com express e typescript

---
+++ Backend/src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.json({
+    message: "API funcionando!",
+  });
+});
+
+app.listen(3333, () => {
+  console.log("Servidor rodando na porta 3333");
+});
