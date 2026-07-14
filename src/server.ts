85cd51c04924a27faa1c1db019f90b6ceeae566c
Vicente Ramos <vicenteprfilho09@gmail.com>
2026-07-13 21:15:47 -0300

    feat: adiciona middleware CORS para origem frontend local

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import express from "express";
+import router from "./routes/stockRoutes.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
+
+app.use(express.json());
+
+app.use(router);
+
+app.get("/", (_req, res) => {
+  return res.json({
+    message: "API funcionando!",
+  });
+});
+
+app.listen(3333, () => {
+  console.log("Servidor rodando na porta 3333");
+});
